56b25f01162ecb72817a10eecee0c8199e8a7b2c
Kai <riselikethemoon@gmail.com>
2015-11-22 09:48:49 -0800

    switched to clump

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ app.secret_key = 'forage_the_things'
 @app.route('/')
 def index():			                                              
 	
-	return render_template('home.html')
+	return render_template('clump.html')
 
 
 if __name__ == "__main__":
