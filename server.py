bd08ebbb710eae5a32c64a4eb44dd7afd14d3f4b
Kai <riselikethemoon@gmail.com>
2015-11-22 09:54:04 -0800

    removed excess form requirements

--- server.py
+++ server.py
@@ -1,6 +1,4 @@
 from flask import Flask, render_template
-from jinja2 import StrictUndefined
-import json
 
 app=Flask(__name__)
 
@@ -14,5 +12,5 @@ def index():
 
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run()
 	
